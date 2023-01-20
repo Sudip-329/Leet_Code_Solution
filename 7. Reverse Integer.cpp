a55abb682a7cf4b0be79abbbac3fed76870cd419
Sudip-329 <108211139+Sudip-329@users.noreply.github.com>
2023-01-21 00:29:27 +0600

    Type : Medium

---
+++ 7. Reverse Integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        while(x!=0)
+        {
+            int y =x%10;
+
+            if((ans>INT_MAX/10) || (ans<INT_MIN/10))  // inside main
+            {
+                return 0;
+            }
+            ans = ans*10 + y;
+            x = x/10;
+        }
+        return ans;
+    }
+};
