a6552b12a357fb393498fe2fb1c4033077cf1c23
Sudip-329 <108211139+Sudip-329@users.noreply.github.com>
2023-02-27 16:04:25 +0530

    Type : Easy

---
+++ 70.Climbing_Stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) 
+    {
+        int p1=1;
+        int p2=1;
+        for(int i=2;i<=n;i++){
+            int ans = p1+p2;
+            p1=p2;
+            p2=ans;
+        }
+        return p2; 
+    }
+};
