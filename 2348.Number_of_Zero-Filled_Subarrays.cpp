2e7d657a3b5f6d15f0f54dc4b7c0cc3dc322d154
Sudip-329 <sudipchakrabarty6@gmail.com>
2023-06-02 21:17:10 +0600

    Type : Medium

---
+++ 2348.Number_of_Zero-Filled_Subarrays.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long count=0,ans=0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i]==0)
+            {
+                count++;
+            }
+            else{
+               ans += (count*(count+1))/2;
+               count = 0;
+            }
+        }
+        if(count!=0)
+        {
+            ans += (count*(count+1))/2;
+        }
+        return ans;    
+    }
+};
