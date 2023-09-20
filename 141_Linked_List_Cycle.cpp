2ef243aef1e60c66188fa49637b09f2f7a1d1360
Sudip-329 <sudipchakrabarty6@gmail.com>
2023-09-21 00:17:55 +0530

    Type : Easy

---
+++ 141_Linked_List_Cycle.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/*class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(fast != NULL && fast->next !=NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if(fast==slow)
+            {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};*/
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode*> st;
+        while(head!=NULL){
+            if(st.find(head)!=st.end()){
+                return true;
+            }
+            st.insert(head);
+            head=head->next;
+        }
+        return false;
+    }
+};
