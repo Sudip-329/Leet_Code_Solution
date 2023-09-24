9208b7936fc44f911085099f02eafe6452a6f2e3
Sudip-329 <sudipchakrabarty6@gmail.com>
2023-09-24 22:49:32 +0530

    Type : Easy

---
+++ 392_Is_Subsequence.cpp
@@ -0,0 +1,53 @@
+/*class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j=0,count=0;
+        for(int i=0; i<t.size(); i++)
+        {
+            if(s[j]==t[i])
+            {
+                count++;
+                j++;
+            }
+        }
+        if(count == (s.size()))
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};*/
+
+// recursion + 2 pointer
+class Solution {
+    int isSubSequence(string& s1, string& s2, int i, int j)
+{
+    if (i == 0 || j == 0) {
+        return 0;
+    }
+    if (s1[i - 1] == s2[j - 1]) {
+        return  1 + isSubSequence(s1, s2, i - 1, j - 1);
+    }
+    else {
+        return  isSubSequence(s1, s2, i, j - 1);
+    }
+}
+ 
+public:
+    bool isSubsequence(string s, string t) {
+         int m = s.size();
+    int n = t.size();
+    if (m > n) {
+        return false;
+    }
+    if (isSubSequence(s, t, m, n) == m) {
+        return true;
+    }
+    else {
+        return false;
+    }
+    }
+};
