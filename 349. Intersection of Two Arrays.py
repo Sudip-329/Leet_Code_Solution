9c8b80390abf5b50dee86981bd038ffff01deaf7
Sudip-329 <sudipchakrabarty6@gmail.com>
2024-03-13 19:38:47 +0530

    13-03-24

---
+++ 349. Intersection of Two Arrays.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return list(set1.intersection(set2))
+    
+# python 3
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return list(set1.intersection(set2))
+
